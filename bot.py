3bc5317057d0960ca2e609a2651a0c33b595d524
Leo Astier <leoastier@MacBook-Pro.local>
2025-08-26 15:48:31 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+from telegram.ext import Application, ChatJoinRequestHandler, ContextTypes
+from telegram import Update
+
+BOT_TOKEN = "8158399936:AAGZaDsO9D-mQ-wXlOMnzR8X-us0xmNThEY"
+
+async def on_join_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    req = update.chat_join_request
+    await context.bot.send_message(
+        chat_id=req.from_user.id,
+        text="👋 Salut ! Voici mon contact direct : @lafameee"
+    )
+
+if __name__ == "__main__":
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(ChatJoinRequestHandler(on_join_request))
+    app.run_polling()
+
